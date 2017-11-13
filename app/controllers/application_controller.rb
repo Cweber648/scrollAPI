89380091d90fa43fdded4bfb9a3b9485721c6c06
Matthew Black <matthewallenblack@gmail.com>
2017-11-12 18:33:01 -0600

    Add delete route, update seeds, update readme.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,4 @@
 class ApplicationController < ActionController::API
   include Response
 
-  def title(article_url)
-    mechanize = Mechanize.new
-    page = mechanize.get(article_url)
-    page.title
-  end
-
-  def body(article_url)
-    mechanize = Mechanize.new
-    page = mechanize.get(article_url)
-
-    if url.hostname == "www.cnn.com"
-      selector = 'div .zn-body__paragraph'
-    elsif url.hostname == "www.nytimes.com"
-      selector = '.story-body-text'
-    elsif url.hostname == "www.foxnews.com"
-      selector = 'p'
-    end
-
-    content = []
-    page.search(selector).each do |div|
-      content << div.text.strip
-    end
-    content
-  end
-
 end
