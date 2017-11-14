6a67b8ae8a7f62b55f9d0910be3b670584427c5d
Matthew Black <matthewallenblack@gmail.com>
2017-11-14 11:27:58 -0600

    Create tests for routes.

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -1,21 +1,43 @@
 require 'rails_helper'
 
+RSpec.describe "Routes", type: :routing do
+
+  describe 'scrollio' do
+    it 'routes /scrollios/:id to scrollios#show for id' do
+      expect(:get => "/scrollios/1").to route_to(
+        :controller => "scrollios",
+        :action => "show",
+        :id => "1"
+      )
+    end
+
+    it 'routes /scrollios/:id to scrollios#destroy for id' do
+      expect(:delete => "/scrollios/1").to route_to(
+        :controller => "scrollios",
+        :action => "destroy",
+        :id => "1"
+      )
+    end
+  end
+
+
+  describe 'article' do
+    it 'routes /scrollios/:scrollio_id/articles to articles#create for scrollio_id' do
+      expect(:post => "/scrollios/1/articles").to route_to(
+        :controller => "articles",
+        :action => "create",
+        :scrollio_id => "1"
+      )
+    end
+
+    it 'routes /scrollios/:scrollio_id/articles/id to articles#destroy for scrollio_id and id' do
+      expect(:delete => "/scrollios/1/articles/3").to route_to(
+        :controller => "articles",
+        :action => "destroy",
+        :scrollio_id => "1",
+        :id => "3"
+      )
+    end
+  end
 
 end
-
-
-# require 'rails_helper'
-
-# RSpec.describe "routing to profiles", :type => :routing do
-#   it "routes /profile/:username to profile#show for username" do
-#     expect(:get => "/profiles/jsmith").to route_to(
-#       :controller => "profiles",
-#       :action => "show",
-#       :username => "jsmith"
-#     )
-#   end
-
-#   it "does not expose a list of profiles" do
-#     expect(:get => "/profiles").not_to be_routable
-#   end
-# end
